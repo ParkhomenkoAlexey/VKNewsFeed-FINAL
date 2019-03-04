0b7b71fae45dabb386c2e7145ec0bb3dde6ad6b6
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2019-03-04 17:16:07 +0300

    Initial Commit

---
+++ VKNewsFeed/Services/FeedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewController.swift
+//  VKNewsFeed
+//
+//  Created by Алексей Пархоменко on 23/02/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.5671284795, green: 0.7945078611, blue: 0.9987251163, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
