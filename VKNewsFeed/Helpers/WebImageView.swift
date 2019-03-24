b14c5708e3eda68490fd3c2582fa2a683e67b015
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2019-03-24 11:21:13 +0300

    13 урок

---
+++ VKNewsFeed/Helpers/WebImageView.swift
@@ -0,0 +1,42 @@
+//
+//  WebImageView.swift
+//  VKNewsFeed
+//
+//  Created by Алексей Пархоменко on 23/03/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class WebImageView: UIImageView {
+    
+    func set(imageURL: String) {
+        guard let url = URL(string: imageURL) else { return }
+        
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            self.image = UIImage(data: cachedResponse.data)
+            //print("from cachе")
+            return
+        }
+        
+        //print("from internet")
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            
+            DispatchQueue.main.async {
+                if let data = data, let response = response {
+                    self?.image = UIImage(data: data)
+                    self?.handleLoadedImage(data: data, response: response)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func handleLoadedImage(data: Data, response: URLResponse) {
+        guard let responseURL = response.url else { return }
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
+        
+    }
+}
